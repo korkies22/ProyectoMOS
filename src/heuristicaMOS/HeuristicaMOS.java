1c293f50811d9ccb92c3afa7c681099dd93f94b4
korkies22 <cm.sarmiento10@uniandes.edu.co>
2018-11-14 11:24:19 -0500

    Proyecto MOS

---
+++ src/heuristicaMOS/HeuristicaMOS.java
@@ -0,0 +1,15 @@
+package heuristicaMOS;
+
+public class HeuristicaMOS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new HeuristicaMOS();
+	}
+	
+	
+	public HeuristicaMOS(){
+		
+	}
+
+}
